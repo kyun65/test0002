e91460da00898e4019bcb62dcf6ff7ac2888263c
きゅん <kyun.anothercastle@gmail.com>
2020-10-02 14:47:17 +0900

    遷移の練習

---
+++ test0002/SegueNextViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SegueNextViewController.swift
+//  test0002
+//
+//  Created by きゅん on 2020/10/02.
+//
+
+import UIKit
+
+class SegueNextViewController: UIViewController {
+
+    @IBOutlet weak var nextLabel: UILabel!
+    
+    var nextText = String()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nextLabel.text = nextText
+
+    }
+    
+
+
+
+}
--- test0002/ViewController.swift
+++ test0002/ViewController.swift
@@ -7,15 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    var text = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+        
+        
+        
+      
     }
 
     
-//    あああ
-
+    @IBAction func next(_ sender: Any) {
+        //ラベルに出力
+        
+        text = textField.text!
+        textLabel.text = text
+        
+        
+        
+        performSegue(withIdentifier: "SegueNext", sender: nil)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        if segue.identifier == "SegueNext" {
+            
+            let nextVC = segue.destination as! SegueNextViewController
+            
+            nextVC.nextText = text
+            
+        }
+    }
+    
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func textFieldAction(_ sender: Any) {
+       
+        
+        
+    }
+    
 }
 
